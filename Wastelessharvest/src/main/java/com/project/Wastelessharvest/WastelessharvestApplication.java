fa72f1c667a3e2b2f985a7d7d5f9aaca8864d409
Josel Xavier <joseltech2002@gmail.com>
2023-06-10 14:56:19 +0530

    spring boot application files generated

---
+++ Wastelessharvest/src/main/java/com/project/Wastelessharvest/WastelessharvestApplication.java
@@ -0,0 +1,13 @@
+package com.project.Wastelessharvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WastelessharvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WastelessharvestApplication.class, args);
+	}
+
+}
