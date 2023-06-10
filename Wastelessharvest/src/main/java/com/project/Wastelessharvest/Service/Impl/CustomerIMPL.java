830cc863c3466c30aa15e6004602e76d282b1b93
Josel Xavier <joseltech2002@gmail.com>
2023-06-10 15:47:29 +0530

    imple strts

--- Wastelessharvest/src/main/java/com/project/Wastelessharvest/Service/Impl/CustomerIMPL.java
+++ Wastelessharvest/src/main/java/com/project/Wastelessharvest/Service/Impl/CustomerIMPL.java
@@ -1,9 +1,13 @@
 package com.project.Wastelessharvest.Service.Impl;
 
 import com.project.Wastelessharvest.Dto.CustomerDTO;
+import com.project.Wastelessharvest.Repositery.CustomerRepo;
 import com.project.Wastelessharvest.Service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class CustomerIMPL implements CustomerService {
+    @Autowired
+    private CustomerRepo customerRepo;
     @Override
     public String addCustomer(CustomerDTO customerDTO) {
         return null;
