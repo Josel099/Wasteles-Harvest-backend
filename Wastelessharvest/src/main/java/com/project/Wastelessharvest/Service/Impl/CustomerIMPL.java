9a20f5d4c1e9be0c4d7f9e9fd9ba01df8f91667c
Josel Xavier <joseltech2002@gmail.com>
2023-06-26 23:59:17 +0530

    edt

--- Wastelessharvest/src/main/java/com/project/Wastelessharvest/Service/Impl/CustomerIMPL.java
+++ Wastelessharvest/src/main/java/com/project/Wastelessharvest/Service/Impl/CustomerIMPL.java
@@ -7,11 +7,9 @@ import com.project.Wastelessharvest.Repositery.CustomerRepo;
 import com.project.Wastelessharvest.Response.LoginResponse;
 import com.project.Wastelessharvest.Service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
